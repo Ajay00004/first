ba606e16abd8bc4bb0dbd296a6d21f66353830bc
Ajay00004 <150114352+Ajay00004@users.noreply.github.com>
2023-11-09 12:51:33 +0530

    Add files via upload

---
+++ ajay.con.py
@@ -0,0 +1,22 @@
+print("wellcome to vivo store")
+print("please select module number of the phone")
+print("1=vivo T1")
+print("2=vivo T1 Pro")
+print("3=vivo T2")
+print("4=vivo T2 Pro")
+print("5=vivo Tx")
+phone=input("please select module number of the phone")
+if(phone=="1"):
+    print("vivo T1 phone cost of $=30")
+elif(phone=="2"):
+    print("vivo T1 Pro phone cost of $=50")
+elif(phone=="3"):
+    print("vivo T2 phone cost of $=35")
+elif(phone=="4"):
+    print("vivo T2 Pro phone cost of $=55")
+elif(phone=="5"):
+    print("vivo Tx phone cost of $=25") 
+else:
+    print(f"sorry we don't have that type of module {phone}")
+    
+
